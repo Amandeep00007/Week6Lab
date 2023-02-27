15a54ad0f7fe972a90e067f760f0a4a7c64fb656
amand <amand@LAPTOP-LK1S03OG>
2023-02-26 23:00:15 -0700

    List Created

--- src/java/Servlets/ShoppingListServlet.java
+++ src/java/Servlets/ShoppingListServlet.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -18,69 +14,75 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ShoppingListServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ShoppingListServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ShoppingListServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        HttpSession session = request.getSession();
+        String action = request.getParameter("action");
+        String username = request.getParameter("name");
+        
+        if(action != null){
+            session.invalidate();
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp")
+                    .forward(request,response);    
+        }else if(username != null){
+            response.sendRedirect("ShoppingList");
+            return;
         }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp")
+                .forward(request,response);     
+        }
+        
+        
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String action = request.getParameter("action");
+        String username = request.getParameter("name");
+        String add = request.getParameter("itemadd");
+        String list = request.getParameter("things");
+        ArrayList<String> itemlist;
+        HttpSession session = request.getSession();
+        
+        if(session.getAttribute("items")== null){
+            itemlist = new ArrayList<>();
+        }else{
+            itemlist = (ArrayList<String>)session.getAttribute("items");
+        }
+        
+            if(action.equals("register")){
+             if(username == null || username.equals("")){
+                request.setAttribute("message", "Enter a name");
+                
+               getServletContext().getRequestDispatcher("/WEB-INF/register.jsp")
+                       .forward(request,response);  
+            }else{
+                session.setAttribute("name", username);
+                getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp")
+                        .forward(request,response);
+            }}
+            if(action.equals("logout")){ itemlist.clear();
+             session.invalidate();
+             getServletContext().getRequestDispatcher("/WEB-INF/register.jsp")
+                     .forward(request,response); }
+            
+            if(action.equals("add")){itemlist.add(add);
+            session.setAttribute("items",itemlist);
+             getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp")
+                     .forward(request,response);}
+            
+            if(action.equals("delete")){itemlist.remove(list);
+            session.setAttribute("items",itemlist);
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp")
+                    .forward(request,response);
+            }    
+        
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp")
+                .forward(request,response);
     }
+    
+} 
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
-}
+   
